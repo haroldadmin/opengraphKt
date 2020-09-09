e4290efdca88f4bfeeb0db4b916b30d111566bce
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2020-09-09 12:10:38 +0530

    feat: Update project to Kotlin 1.4.0

--- buildSrc/src/main/kotlin/Libraries.kt
+++ buildSrc/src/main/kotlin/Libraries.kt
@@ -1,11 +1,11 @@
 object Libraries {
 
-    const val jsoup = "org.jsoup:jsoup:1.12.1"
-    const val mockWebServer = "com.squareup.okhttp3:mockwebserver:4.2.1"
+    const val jsoup = "org.jsoup:jsoup:1.13.1"
+    const val mockWebServer = "com.squareup.okhttp3:mockwebserver:4.8.1"
 
     object Kotlin {
         const val stdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${ProjectProperties.kotlinVersion}"
-        const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2"
+        const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9"
         const val test = "org.jetbrains.kotlin:kotlin-test:${ProjectProperties.kotlinVersion}"
         const val testJunit = "org.jetbrains.kotlin:kotlin-test-junit:${ProjectProperties.kotlinVersion}"
     }
