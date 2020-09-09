78ff4c2cc8a3b692dac4db3405e30490ec5c3ffa
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2020-09-09 12:12:29 +0530

    misc: Prepare for 1.0 release

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -1,6 +1,6 @@
 object ProjectProperties {
     const val applicationId = "com.haroldadmin.opengraphKt"
-    const val versionName = "0.0.1"
+    const val versionName = "1.0.0"
 
     const val kotlinVersion = "1.4.0"
 }
